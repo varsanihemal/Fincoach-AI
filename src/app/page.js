ce08462b3ec160eb72178dd4f5c74bac2978d526
varsanihemal <hemalvarsani123@gmail.com>
2026-04-02 15:19:05 -0500

    feat: initial commit

---
+++ src/app/page.js
@@ -0,0 +1,6 @@
+export default function Home() {
+  return <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <h1 className="text-4xl font-bold mb-4">Welcome to Next.js!</h1>
+    <p className="text-lg text-gray-600 mb-8">Get started by editing <code className="bg-gray-200 px-1 py-0.5 rounded">src/app/page.js</code></p>
+  </div>
+}
